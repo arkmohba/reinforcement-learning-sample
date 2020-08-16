286b5e9eb6643d31394cc04d0be339a7b20f136d
ohba.masao <24daikaityou@gmail.com>
2020-08-17 00:02:28 +0900

    ヘッダに書かれていたソースをcppファイルに分離

--- particles/bnatsver7/disp.h
+++ particles/bnatsver7/disp.h
@@ -13,47 +13,13 @@ const cv::Scalar COLOR_GREEN = {0, 255, 0};
 const cv::Scalar COLOR_RED = {0, 0, 255};
 
 void draw_ball(cv::Mat img, int x, int y, int r, cv::Scalar color,
-               int thickness) {
-  cv::circle(img, cv::Point2i(x, y), r, color, thickness);
-}
+               int thickness);
 
-void draw_ball_normal(cv::Mat img, double x, double y, double r) {
-  auto color = COLOR_BLACK;
-  int thickness = 1;
-  draw_ball(img, x, y, r, color, thickness);
-}
+void draw_ball_normal(cv::Mat img, double x, double y, double r);
 
-void draw_ball_another(cv::Mat img, double x, double y, double r) {
-  auto color = COLOR_RED;
-  int thickness = -1;
-  draw_ball(img, x, y, r, color, thickness);
-}
+void draw_ball_another(cv::Mat img, double x, double y, double r);
 
-void rect(cv::Mat img, int x, int y, int w, int h, cv::Scalar color) {
-  cv::rectangle(img, cv::Point(x, y), cv::Point(x + w, y + h), color, 3, 4);
-}
+void rect(cv::Mat img, int x, int y, int w, int h, cv::Scalar color);
 
 cv::Mat udisp(Xsdata *xdata, int particles_num, int height, int width,
-              int dcell) {
-  // background
-  cv::Mat img = cv::Mat::ones(height, width, CV_8UC3);
-  img = cv::Scalar(255, 255, 255);  //画面を白で塗りつぶし
-
-  /*draw walls*/
-  rect(img, 0.0, 0.0, dcell, height, COLOR_BLACK);            // left wall
-  rect(img, width - dcell, 0.0, dcell, height, COLOR_BLACK);  // right wall
-  rect(img, 0.0, 0.0, width, dcell, COLOR_BLACK);             // top
-  rect(img, 0.0, height - dcell - base, width, dcell + base,
-       COLOR_BLACK);  // bottom
-
-  /*draw the particles*/
-  for (int p_idx = 1; p_idx < particles_num; p_idx++){
-    draw_ball_normal(img, xdata[p_idx].x, xdata[p_idx].y,
-                     xdata[p_idx].r);  // display the particles
-  }
-  // draw the brazil nat.
-  int p_idx = 0;
-  draw_ball_another(img, xdata[p_idx].x, xdata[p_idx].y, xdata[p_idx].r);
-
-  return img;
-}
+              int dcell, double base);
--- particles/bnatsver7/main.cpp
+++ particles/bnatsver7/main.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 
 #include <cstdio>
+#include <iostream>
 #include <string>
 
 #include "culc.h"
@@ -9,47 +10,51 @@
 
 using namespace std;
 
-int count = 0;
-
 const int SHOW_FOR_EACH = 30;
 
-void init(Xsdata *xdata, string window_name) {
-  datainit(xdata);
+void init(Xsdata *xdata, double base, double base2, string window_name) {
+  datainit(xdata, base, base2);
   cv::namedWindow(window_name);
 }
 
-void update(Xsdata *xdata, string window_name) {
-  static double time = 0;
+void update(Xsdata *xdata, double &t, double &base, double &base2,
+            string window_name) {
+  // static double time = 0;
 
   for (int i = 0; i < SHOW_FOR_EACH; i++) {
     // 床のアップデート
-    time += dt;
-    if (time > 10 / nyu) time = 0;
-    base = A * pow(sin(2 * M_PI * nyu * time), 1);
+    // t;
+    t = t + dt;
+    // cout << t << endl;
+    if (t > 10 / nyu) t = 0;
+    base = A * pow(sin(2 * M_PI * nyu * t), 1);
 
     // base2 = base*B / A;
-    interaction(xdata);
+    interaction(xdata, base, base2);
     partupdate(xdata);
   }
-  auto img = udisp(xdata, num_particles2, height, width, dcell);
+  auto img = udisp(xdata, num_particles2, height, width, dcell, base);
   cv::imshow(window_name, img);
 }
 
 int main(int argc, char *argv[]) {
   Xsdata *xdata = new Xsdata[num_particles];
   string window_name = "";
-  init(xdata, window_name);
+  double base, base2, t;
+  base = 0;
+  base2 = 0;
+  init(xdata, base, base2, window_name);
 
   int key;
-  double time = 0;
+  t = 0;
   while (1) {
     int key;
-    update(xdata, window_name);
+    update(xdata, t, base, base2, window_name);
     key = cv::waitKey(1);
     // 'Esc'が押された場合に終了
     if (key == 27) break;
   }
-	cv::destroyWindow(window_name);
+  cv::destroyWindow(window_name);
 
   delete[] xdata;
   return 0;
