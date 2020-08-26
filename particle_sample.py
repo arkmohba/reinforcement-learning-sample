aa255868159f47ad3cd3b50265751c72b13b71a9
ohba.masao <24daikaityou@gmail.com>
2020-08-26 23:54:52 +0900

    サンプルで動画出力する方式に変更

--- particle_sample.py
+++ particle_sample.py
@@ -4,7 +4,7 @@ import time
 from tqdm import tqdm
 import random
 import math
-
+import cv2
 
 def main():
     env_name = "Bnuts-v0"
@@ -13,25 +13,36 @@ def main():
     t = 0
     dt = 0.1
     action = 0
-    for _ in tqdm(range(10000)):
+
+    frame_rate = 30  # フレームレート
+    size = env.observation_space.shape[:2]  # 動画の画面サイズ
+    size = (size[1], size[0])  # width, heightの順
+
+    fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')  # ファイル形式(ここではmp4)
+    writer = cv2.VideoWriter('./output.mp4', fmt, frame_rate, size)  # ライター作成
+
+
+    for _ in tqdm(range(1000)):
         env.render()
         # 直接ランダムだと壁に合わせて動く前に壁が戻ってしまってうまく動かない
         # action = random.randrange(0, action_size)
         
-        # 振動させると無難（もともとの使用）
-        # action = action_size * math.sin(t)
-        # t += dt
+        # 振動させると無難（もともとの仕様）
+        action = action_size * math.sin(t)
+        t += dt
 
         # ちょっとずつランダムにずらすとそれなりに振動する
         # action += random.randrange(-10, 10)
         # action = min(max(action, 0), action_size - 1)
 
         # 階段状にランダムにしたものの普通にランダムにした場合と変わらなかった。
-        action = random.randrange(0, env.max_diff+ 1) * env.separate
+        # action = random.randrange(0, env.max_diff+ 1) * env.separate
         # print(action)
-        env.step(action)
+        obs, _, _, _ = env.step(action)
+        writer.write(obs)  # 画像を1フレーム分として書き込み
         # time.sleep(0.001)
 
+    writer.release()  # ファイルを閉じるs
 
 if __name__ == "__main__":
     main()
