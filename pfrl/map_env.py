c7f2ea59eaae3109b5d5fe4ac20e50f86d895a1d
ohba.masao <24daikaityou@gmail.com>
2021-01-26 10:40:05 +0900

    ENVを改良

--- pfrl/map_env.py
+++ pfrl/map_env.py
@@ -17,14 +17,20 @@ class MapRootEnv(gym.Env):
         'R',
         'N'
     ]
-    WIDTH = 100
-    HEIGHT = 100
-    MAX_STEPS = 100
+    WIDTH = 50
+    HEIGHT = 50
+    MAX_STEPS = 499
     RESTORE_POINTS = 10
     BLUE = np.array([255, 0, 0])
     GREEN = np.array([0, 255, 0])
     RED = np.array([0, 0, 255])
     BLACK = np.array([0, 0, 0])
+    NEXTS = [
+        np.array([-1, 0]),
+        np.array([0, 1]),
+        np.array([1, 0]),
+        np.array([0, -1])
+    ]
 
     def __init__(self):
         self.steps = 0
@@ -49,7 +55,7 @@ class MapRootEnv(gym.Env):
         )
 
         # 報酬の最大最小
-        self.reward_range = [-1, 100.]
+        self.reward_range = [-1.7, 11.]
         self.reset()
         pass
 
@@ -76,7 +82,7 @@ class MapRootEnv(gym.Env):
         for _ in range(self.RESTORE_POINTS):
             r_x, r_y = self.random_pos()
             root_map[r_y][r_x][2] = 1
-        return np.array([s_y, s_x]), root_map
+        return np.array([s_y, s_x]), np.array([s_y, s_x]), np.array([g_y, g_x]), root_map
 
     def map_to_image(self, root_map):
         img = np.ones([self.HEIGHT, self.WIDTH, 3]) * 255
@@ -93,20 +99,12 @@ class MapRootEnv(gym.Env):
     def reset(self):
         self.steps = 0
         self.done = False
-        self.pos, self.map = self.generate_map()
+        self.pos, self.start, self.goal, self.map = self.generate_map()
         self.trail = [self.pos]
-        return self.map_to_image(self.map)
+        return self.map.transpose(2, 0, 1).astype(np.float32)
 
     def next_pos(self, index: int, pos: np.ndarray):
-        next_pos = pos.copy()
-        if index == 0:  # 画面上
-            next_pos[0] -= 1
-        elif index == 1:  # 画面右
-            next_pos[1] += 1
-        elif index == 2:  # 画面下
-            next_pos[0] += 1
-        else:  # 画面左
-            next_pos[1] -= 1
+        next_pos = pos + self.NEXTS[index]
         # 画面外に出ていないかチェック
         next_pos[0] = np.clip(next_pos[0], 0, self.HEIGHT-1)
         next_pos[1] = np.clip(next_pos[1], 0, self.WIDTH-1)
@@ -118,18 +116,22 @@ class MapRootEnv(gym.Env):
         if state[0] == 1:
             # 開始地点に戻ってきたらマイナス
             rewards -= 1
-        if state[1] == 1:
+        elif state[1] == 1:
             # 終了地点に戻ってきたらプラス
-            rewards += 10
+            rewards += 100
         if state[2] == 1:
             # 補給地点に来れたらプラス
             # TODO 時間変動をつける
-            rewards += 1
+            rewards += 0.15
+        if np.linalg.norm(self.goal - next_pos) < np.linalg.norm(self.goal - pos):
+            # ゴールに近づいたらプラス
+            rewards += 0.05
 
         # 移動のコストを追加
         # TODO できれば道の高低差をコストに入れたい
         diff = next_pos - pos
-        if diff.sum() > 0:
+        if diff.sum() < 1:
+            # 移動していなければマイナス
             rewards -= 0.5
 
         # 時間経過で少しマイナス
@@ -143,7 +145,7 @@ class MapRootEnv(gym.Env):
         self.map[next_pos[0]][next_pos[1]][3] = 1
 
     def _is_done(self, pos):
-        if self.map[pos[0]][pos[1]][1] == 1:
+        if np.all(pos == self.goal):
             # ゴールに着いたら
             return True
         elif self.steps > self.MAX_STEPS:
@@ -154,6 +156,7 @@ class MapRootEnv(gym.Env):
 
     def step(self, action):
         """actionは数字"""
+        self.steps += 1
         # 次の位置を確認
         next_pos = self.next_pos(action, self.pos)
         # 報酬を計算
@@ -168,20 +171,21 @@ class MapRootEnv(gym.Env):
 
         # 終了したかどうか
         self.done = self._is_done(self.pos)
-        return observation, reward, self.done, {}
+        return observation.transpose(2, 0, 1).astype(np.float32), reward, self.done, {}
 
     def render(self, mode='human', close=False):
         img = self.map_to_image(self.map)
-        img = cv2.resize(img, (img.shape[1]*4, img.shape[0]*4), interpolation=cv2.INTER_NEAREST)
-        cv2.imshow('image', img)
-    
-    def render_trailed_image(self):
-        img = self.map_to_image(self.map)
-        for y,x in self.trail:
-            img[y][x] = self.BLACK
-        img = cv2.resize(img, (img.shape[1]*4, img.shape[0]*4), interpolation=cv2.INTER_NEAREST)
+        img = cv2.resize(
+            img, (img.shape[1]*4, img.shape[0]*4), interpolation=cv2.INTER_NEAREST)
         cv2.imshow('image', img)
 
+    def render_trailed_image(self):
+        img = self.map_to_image(self.map)
+        for y, x in self.trail:
+            img[y][x] = self.BLACK
+        img = cv2.resize(
+            img, (img.shape[1]*4, img.shape[0]*4), interpolation=cv2.INTER_NEAREST)
+        cv2.imshow('image', img)
 
     def _close(self):
         cv2.destroyAllWindows()
